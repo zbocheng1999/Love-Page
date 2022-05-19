fa0d3270518f2d2cb88e5dbe94e78e3787c2a11d
zbocheng1999 <76125413+zbocheng1999@users.noreply.github.com>
2022-05-19 03:46:36 -0700

    Update talk.js

--- js/talk.js
+++ js/talk.js
@@ -3,12 +3,12 @@ new TypeIt("#tqyxhj", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("TQY && XHJ")
+    .type("ZBC && ZXY")
     .pause(2000)
     .delete(null, {
         delay: 500
     })
-    .type("一起走过的第二个520...")
+    .type("To be continued...")
     .pause(3000)
     .go();
 
