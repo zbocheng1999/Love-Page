b4085ef403a47ddce5a88f7e96ec9cb8f061fd83
zbocheng1999 <76125413+zbocheng1999@users.noreply.github.com>
2022-05-19 17:43:54 -0700

    Update lovetime.js

--- js/lovetime.js
+++ js/lovetime.js
@@ -5,6 +5,7 @@ function lovetime() {
     var hours = minutes * 60
     var days = hours * 24
     var years = days * 365
+
     var today = new Date()
     var todayYear = today.getFullYear()
     var todayMonth = today.getMonth()
@@ -12,17 +13,20 @@ function lovetime() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    // 修改这里时间即可 我的是2018.11.19
-    var t1 = Date.UTC(2022, 5, 14, 00, 00, 00)
-    var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
-    var diff = t2 - t1
+
+// 时间作差
+    var t1 = new Date(2022, 4, 14, 8, 00, 00)
+    var t2 = new Date(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
+    var diff = t2.getTime() - t1.getTime()
+
     var diffYears = Math.floor(diff / years)
+    var diffDays = Math.floor(diff - diffYears * years)
     var diffDays = Math.floor((diff / days) - diffYears * 365)
     var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours)
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
         minutes) / seconds)
-document.getElementById("lovetime").innerHTML = "今天时间是 " + todayYear + "年" + todayMonth + "月" + todayDate + "天" +
-        todayHour + "小时" + todayMinute + "分钟" + todaySecond + "秒啦"
+    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + diffYears + "年" + diffDays + "天" +
+        diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
 }
 lovetime()
